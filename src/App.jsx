dc6394c735c0a0da9d3346afdef0e25775ec59ef
aylindoganer <doganeraylinn@gmail.com>
2022-10-27 16:13:58 +0300

    end game function added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ export default function App() {
 
   const [dice, setDice] = useState(allNewDice())
 
+  const [tenzies, setTenzies] = React.useState(false)
+    
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+        setTenzies(true)
+        console.log("You won!")
+    }
+  }, [dice])
+
   function generateNewDie() {
     return {
         value: Math.ceil(Math.random() * 6),
