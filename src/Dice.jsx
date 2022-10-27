5d7ccb5411ee711ec4d301bb5fb65ee2c0e25265
aylindoganer <doganeraylinn@gmail.com>
2022-10-27 12:01:54 +0300

    created the dice component

---
+++ src/Dice.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function Dice (props) {
+    return (
+        <div className="dice-face">
+          <h2 className="dice-num">{props.value}</h2>
+        </div>
+    )
+}
